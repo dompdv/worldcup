6660bff26c7edb09850725ea0b129d17854323cd
Dominique Poudevigne <dompdv@yahoo.fr>
2018-04-21 19:04:24 +0200

    Affichage correct des simulations avec analyse des performances

---
+++ data_2018.py
@@ -0,0 +1,143 @@
+
+def matches():
+    return [
+        {'date': '2018-06-14', 'group': 'A', 'match': 'A1', 'team1': 'Russia', 'team2': 'Saudi Arabia'},
+        {'date': '2018-06-15', 'group': 'B', 'match': 'B1', 'team1': 'Morocco', 'team2': 'Iran'},
+        {'date': '2018-06-15', 'group': 'B', 'match': 'B2', 'team1': 'Portugal', 'team2': 'Spain'},
+        {'date': '2018-06-15', 'group': 'A', 'match': 'A2', 'team1': 'Egypt', 'team2': 'Uruguay'},
+        {'date': '2018-06-16', 'group': 'C', 'match': 'C1', 'team1': 'France', 'team2': 'Australia'},
+        {'date': '2018-06-16', 'group': 'C', 'match': 'C2', 'team1': 'Peru', 'team2': 'Denmark'},
+        {'date': '2018-06-16', 'group': 'D', 'match': 'D1', 'team1': 'Argentina', 'team2': 'Iceland'},
+        {'date': '2018-06-16', 'group': 'D', 'match': 'D2', 'team1': 'Croatia', 'team2': 'Nigeria'},
+        {'date': '2018-06-17', 'group': 'F', 'match': 'F1', 'team1': 'Germany', 'team2': 'Mexico'},
+        {'date': '2018-06-17', 'group': 'E', 'match': 'E1', 'team1': 'Costa Rica', 'team2': 'Serbia'},
+        {'date': '2018-06-17', 'group': 'E', 'match': 'E2', 'team1': 'Brazil', 'team2': 'Switzerland'},
+        {'date': '2018-06-18', 'group': 'G', 'match': 'G1', 'team1': 'Tunisia', 'team2': 'England'},
+        {'date': '2018-06-18', 'group': 'F', 'match': 'F2', 'team1': 'Sweden', 'team2': 'Korean Republic'},
+        {'date': '2018-06-18', 'group': 'G', 'match': 'G2', 'team1': 'Belgium', 'team2': 'Panama'},
+        {'date': '2018-06-19', 'group': 'A', 'match': 'A3', 'team1': 'Russia', 'team2': 'Egypt'},
+        {'date': '2018-06-19', 'group': 'H', 'match': 'H1', 'team1': 'Colombia', 'team2': 'Japan'},
+        {'date': '2018-06-19', 'group': 'H', 'match': 'H2', 'team1': 'Poland', 'team2': 'Senegal'},
+        {'date': '2018-06-20', 'group': 'B', 'match': 'B3', 'team1': 'Portugal', 'team2': 'Morocco'},
+        {'date': '2018-06-20', 'group': 'A', 'match': 'A4', 'team1': 'Uruguay', 'team2': 'Saudi Arabia'},
+        {'date': '2018-06-20', 'group': 'B', 'match': 'B4', 'team1': 'Iran', 'team2': 'Spain'},
+        {'date': '2018-06-21', 'group': 'C', 'match': 'C3', 'team1': 'Denmark', 'team2': 'Australia'},
+        {'date': '2018-06-21', 'group': 'D', 'match': 'D3', 'team1': 'Argentina', 'team2': 'Croatia'},
+        {'date': '2018-06-21', 'group': 'C', 'match': 'C4', 'team1': 'France', 'team2': 'Peru'},
+        {'date': '2018-06-22', 'group': 'E', 'match': 'E3', 'team1': 'Brazil', 'team2': 'Costa Rica'},
+        {'date': '2018-06-22', 'group': 'D', 'match': 'D4', 'team1': 'Nigeria', 'team2': 'Iceland'},
+        {'date': '2018-06-22', 'group': 'E', 'match': 'E4', 'team1': 'Serbia', 'team2': 'Switzerland'},
+        {'date': '2018-06-23', 'group': 'F', 'match': 'F3', 'team1': 'Korean Republic', 'team2': 'Mexico'},
+        {'date': '2018-06-23', 'group': 'F', 'match': 'F4', 'team1': 'Germany', 'team2': 'Sweden'},
+        {'date': '2018-06-23', 'group': 'G', 'match': 'G3', 'team1': 'Belgium', 'team2': 'Tunisia'},
+        {'date': '2018-06-24', 'group': 'H', 'match': 'H3', 'team1': 'Poland', 'team2': 'Colombia'},
+        {'date': '2018-06-24', 'group': 'G', 'match': 'G4', 'team1': 'England', 'team2': 'Panama'},
+        {'date': '2018-06-24', 'group': 'H', 'match': 'H4', 'team1': 'Japan', 'team2': 'Senegal'},
+        {'date': '2018-06-25', 'group': 'A', 'match': 'A5', 'team1': 'Saudi Arabia', 'team2': 'Egypt'},
+        {'date': '2018-06-25', 'group': 'B', 'match': 'B5', 'team1': 'Spain', 'team2': 'Morocco'},
+        {'date': '2018-06-25', 'group': 'B', 'match': 'B6', 'team1': 'Iran', 'team2': 'Portugal'},
+        {'date': '2018-06-25', 'group': 'A', 'match': 'A6', 'team1': 'Uruguay', 'team2': 'Russia'},
+        {'date': '2018-06-26', 'group': 'D', 'match': 'D5', 'team1': 'Nigeria', 'team2': 'Argentina'},
+        {'date': '2018-06-26', 'group': 'D', 'match': 'D6', 'team1': 'Iceland', 'team2': 'Croatia'},
+        {'date': '2018-06-26', 'group': 'C', 'match': 'C5', 'team1': 'Denmark', 'team2': 'France'},
+        {'date': '2018-06-26', 'group': 'C', 'match': 'C6', 'team1': 'Australia', 'team2': 'Peru'},
+        {'date': '2018-06-27', 'group': 'E', 'match': 'E5', 'team1': 'Serbia', 'team2': 'Brazil'},
+        {'date': '2018-06-27', 'group': 'E', 'match': 'E6', 'team1': 'Switzerland', 'team2': 'Costa Rica'},
+        {'date': '2018-06-27', 'group': 'F', 'match': 'F5', 'team1': 'Korean Republic', 'team2': 'Germany'},
+        {'date': '2018-06-27', 'group': 'F', 'match': 'F6', 'team1': 'Mexico', 'team2': 'Sweden'},
+        {'date': '2018-06-28', 'group': 'G', 'match': 'G5', 'team1': 'England', 'team2': 'Belgium'},
+        {'date': '2018-06-28', 'group': 'H', 'match': 'H5', 'team1': 'Senegal', 'team2': 'Colombia'},
+        {'date': '2018-06-28', 'group': 'H', 'match': 'H6', 'team1': 'Japan', 'team2': 'Poland'},
+        {'date': '2018-06-28', 'group': 'G', 'match': 'G6', 'team1': 'Panama', 'team2': 'Tunisia'},
+
+        {'date': '2018-06-30', 'match': '8AB', 'group': ''},
+        {'date': '2018-06-30', 'match': '8CD', 'group': ''},
+        {'date': '2018-07-01', 'match': '8BA', 'group': ''},
+        {'date': '2018-07-01', 'match': '8DC', 'group': ''},
+        {'date': '2018-07-02', 'match': '8EF', 'group': ''},
+        {'date': '2018-07-02', 'match': '8GH', 'group': ''},
+        {'date': '2018-07-03', 'match': '8FE', 'group': ''},
+        {'date': '2018-07-03', 'match': '8HG', 'group': ''},
+
+        {'date': '2018-07-06', 'match': '4ABCD', 'group': ''},
+        {'date': '2018-07-06', 'match': '4DCBA', 'group': ''},
+
+        {'date': '2018-07-07', 'match': '4FEHG', 'group': ''},
+        {'date': '2018-07-07', 'match': '4EFGH', 'group': ''},
+
+        {'date': '2018-07-10', 'match': '2ABCDDCBA', 'group': ''},
+        {'date': '2018-07-11', 'match': '2EFGHFEHG', 'group': ''},
+
+        {'date': '2018-07-14', 'match': '1M', 'group': ''},
+        {'date': '2018-07-15', 'match': '1', 'group': ''},
+    ]
+
+
+def match_codes():
+    return [r['match'] for r in matches()]
+
+
+def teams():
+    t = set(r['team1'] for r in matches() if 'team2' in r) | set(r['team2'] for r in matches() if 'team1' in r)
+    return sorted(list(t))
+
+
+def results():
+    return {r['match']:  r['gd'] for r in matches() if 'gd' in r}
+
+def groups():
+    group_names = set(r['group'] for r in matches() if r['group'] != '')
+    groups = {g:set() for g in group_names}
+    for r in matches():
+        if r['group'] != '':
+            groups[r['group']].add(r['team1'])
+            groups[r['group']].add(r['team2'])
+    return groups
+
+def elo_scores():
+    return {
+        'Brazil': 2131,
+        'Germany':2092,
+        'Spain': 2048,
+        'Argentina':1985,
+        'France': 1984,
+        'Portugal':1975,
+        'England':1941,
+        'Colombia': 1935,
+        'Belgium': 1931,
+        'Peru': 1906,
+        'Uruguay':1890,
+        'Switzerland': 1879,
+        'Mexico': 1859,
+        'Croatia': 1853,
+        'Denmark': 1843,
+        'Poland': 1831,
+        'Sweden': 1796,
+        'Iran': 1793,
+        'Iceland': 1787,
+        'Serbia': 1770,
+        'Senegal': 1747,
+        'Korean Republic': 1746,
+        'Costa Rica': 1745,
+        'Australia': 1714,
+        'Morocco': 1711,
+        'Nigeria': 1699,
+        'Japan': 1693,
+        'Russia': 1685,
+        'Panama': 1669,
+        'Tunisia': 1649,
+        'Egypt': 1646,
+        'Saudi Arabia': 1568,
+    }
+
+
+def qualification_teams():
+    t = set(r['team1'] for r in matches()) | set(r['team2'] for r in qualifications())
+    return sorted(list(t))
+
+
+def qualifications_not_friendly():
+    return [m for m in qualifications() if m['type'] != 'Friendly']
+
+def qualifications():
+    return []
---
+++ worldcup_2018.py
@@ -0,0 +1,237 @@
+from math import sqrt, floor
+import operator
+import data_2018
+from model_base import Model_base, print_p, draw_ps
+import random
+import collections
+
+def fire_once(model,teams, groups, matches, match_codes, given, printing=False):
+    # Calcule le match suivant après les pools
+    def find_next_match(code, results, team_group_score):
+        def group_winner(group, position):
+            def order_teams(t):
+                return (t[1]['points'], t[1]['gd'])
+            g_scores = {team: team_group_score[team] for team in groups[group]}
+            sorted_g = sorted(g_scores.items(), key=order_teams)
+            team, _ = sorted_g[-position]
+            return team
+
+        def match_winner(result):
+            return result['team1'] if result['gd'] > 0 else result['team2']
+
+        def match_loser(result):
+            return result['team2'] if result['gd'] > 0 else result['team1']
+
+        if code[0] == "8":
+            team1, team2 = group_winner(code[1],1), group_winner(code[2], 2)
+
+        if code[0] == "4":
+            team1, team2 = match_winner(results["8" + code[1:3]]), match_winner(results["8" + code[3:]])
+
+        if code[0] == "2":
+            team1, team2 = match_winner(results["4" + code[1:5]]), match_winner(results["4" + code[5:]])
+
+        if code == "1":
+            team1, team2 = match_winner(results["2ABCDDCBA"]), match_winner(results["2EFGHFEHG"])
+
+        if code == "1M":
+            team1, team2 = match_loser(results["2ABCDDCBA"]), match_loser(results["2EFGHFEHG"])
+        return team1, team2
+    '''
+        # garde tous les matchs de pool
+        for c in list(given.keys()):
+            pool = ord(c[0]) > 64
+            if not pool:
+                del given[c]
+        #given = {}
+    '''
+    numbered_teams ={ team:number for number,team in enumerate(teams)}
+    team_group_score = { team: {'points': 0, 'gd': 0} for team in teams}
+    results = {}
+    for match in matches:
+        code = match['match']
+        pool = ord(code[0]) > 64
+        m = {'date': match['date'], 'match': code, 'pool': pool, 'ending':'std'}
+        if code in given:
+            m['given'] = True
+            m['gd'] = match['gd']
+            m['team1'] = match['team1']
+            m['team2'] = match['team2']
+            team1 = numbered_teams[m['team1']]
+            team2 = numbered_teams[m['team2']]
+            score = m['gd']
+            model.account_for((team1, team2, score))
+        else:
+            m['given'] = False
+            # Determination des équipes
+            if pool:
+                m['team1'] = match['team1']
+                m['team2'] = match['team2']
+            else:
+                m['team1'], m['team2'] = find_next_match(code, results, team_group_score)
+
+            team1 = numbered_teams[m['team1']]
+            team2 = numbered_teams[m['team2']]
+            # Determination du score
+            proba = model.proba_score(team1, team2)
+            m['proba'] = proba
+            # Randomly choose an outcome
+            #tirages = [draw_ps(proba) for _ in range(10000)]
+            #print(collections.Counter(tirages))
+            score = draw_ps(proba)
+            if score == 0 and not pool:
+                # Pas de nul possible après les Pools, on relance
+                m['ending'] = 'prolong'
+                score = draw_ps(proba)
+                # Penalty : 1 chance sur 2
+                if score == 0:
+                    m['ending'] = 'penalties'
+                    score = random.choice([-1,1])
+            m['gd'] = score
+            model.account_for((team1, team2, score))
+        if score > 0:
+            m['winner'] = m['team1']
+        elif score < 0:
+            m['winner'] = m['team2']
+        else:
+            m['winner'] = None
+        # Gestion des points la phase Pool
+        if pool:
+            if score > 0:
+                team_group_score[m['team1']]['points'] += 3
+                team_group_score[m['team1']]['gd'] += score
+            elif score == 0:
+                team_group_score[m['team1']]['points'] += 1
+                team_group_score[m['team2']]['points'] += 1
+            else:
+                team_group_score[m['team2']]['points'] += 3
+                team_group_score[m['team2']]['gd'] += (-score)
+            m['team1_score'] = team_group_score[m['team1']]['points']
+            m['team2_score'] = team_group_score[m['team2']]['points']
+        if printing:
+            print(m)
+            model.print(teams)
+
+        results[code] = m
+    return results, team_group_score
+
+def account_for_history(model, matches, teams):
+    numbered_teams ={ team:number for number,team in enumerate(teams)}
+    filtered = set()
+    for i, match in enumerate(matches):
+        l1 = numbered_teams[match['team1']]
+        l2 = numbered_teams[match['team2']]
+        score = match['gd']
+        if len(filtered) > 0:
+            display_list = filtered.copy()
+            display_list.add(l1)
+            display_list.add(l2)
+            if l1 in filtered or l2 in filtered:
+                print(i, match)
+                print("Before")
+                model.print(teams, keep=display_list)
+                model.account_for((l1, l2, score))
+                print("After")
+                model.print(teams, keep=display_list)
+        else:
+            print(i, match)
+            model.account_for((l1, l2, score))
+    #model.print(teams)
+
+def print_results(results, matches, groups, teams, team_group_score):
+    print("Date       |    Code    | Pool | Team1             | Team2             | Score | Winner             |")
+    for m in matches:
+        code = m['match']
+        match = results[code]
+        gd = match['gd']
+        team1 = match['team1']
+        team2 = match['team2']
+        winner = ''
+        if not match['pool']:
+            winner = team1 if gd>0 else team2
+        print("{:^11}|{:^12}|{:^6}|{:^19}|{:^19}|{:^7}|{:^19}".format(
+            m['date'], code, match['pool'], team1, team2, gd, winner)
+        )
+    print()
+
+def print_team_score(team_score):
+    keys = False
+    for t, v in team_score.items():
+        if not keys:
+            keys = list(v.keys())
+            print("{:^20}|".format("Teams"), end='')
+            [print("{:^5}|".format(k), end='') for k in keys]
+            print()
+        print("{:^20}|".format(t), end='')
+        [print("{:^5d}|".format(v[k]), end='') for k in keys]
+        print()
+
+
+# Programme Principal
+n_buckets = 6
+rho = 0.5
+bucket_per_gd = 1
+
+teams = data_2018.teams()
+matches = data_2018.matches()
+match_codes = data_2018.match_codes()
+given = data_2018.results()
+groups = data_2018.groups()
+n_people = len(teams)
+
+elo_scores = data_2018.elo_scores()
+# Ai calculé que 500 points correspondent à 3 points d'ecart au total
+max_elo_score = max(elo_scores.values())
+min_elo_score = min(elo_scores.values())
+delta_elo = max_elo_score - min_elo_score
+mean_elo = (max_elo_score + min_elo_score) / 2.0
+# On repositionne linéirement les Ecarts en fonction du ELO
+elo_scores = {k: (v - mean_elo) * 2.5 / 500 for k,v in elo_scores.items()}
+
+def elo_function(avg, x):
+    return 0.1 ** (abs(x-avg))
+#Calage des probabilités en fonction des ELOs
+probabilities = {}
+for team in elo_scores.keys():
+    avg = elo_scores[team]
+    proba = {k: (elo_function(avg, k-0.5) + elo_function(avg, k+0.5))/2.0 for k in range(-n_buckets, n_buckets+1)}
+    s = sum(proba.values())
+    proba = {k:v/s for k,v in proba.items()}
+    probabilities[team] = proba
+
+
+
+model = Model_base(n_people, n_buckets, s_max=floor((2*n_buckets+1)/bucket_per_gd)+1, options={'rho': rho, 'bucket_per_gd': bucket_per_gd} )
+# récupère les etats historiques
+model.probabilities = [probabilities[teams[i]] for i in range(len(model.probabilities))]
+model.update_stats()
+model.adjust_mean()
+model.print(teams)
+
+probabilities = model.probabilities.copy()
+
+team_score = {team: {"1": 0, "1M": 0, "2": 0, "4": 0, "8": 0, "q8": 0} for team in teams}
+
+for _ in range(10000):
+    model.probabilities = probabilities.copy()
+    model.update_stats()
+    results, team_group_score = fire_once(model,teams, groups, matches, match_codes, given)
+    print("Results")
+    print_results(results, matches, groups, teams, team_group_score)
+#    model.print(teams)
+    team1, team2 = results["1"]['team1'], results["1"]['team2']
+    gd = results["1"]['gd']
+    winner = team1 if gd > 0 else team2
+    print("Finale {} / {} : {} Winner {}".format(team1, team2, gd, winner))
+    for code,m in results.items():
+        c = code[0]
+        if ord(c)<65:
+            if c == "8":
+                # Les équipes ont passé les pool
+                team_score[m['team1']]['q8'] += 1
+                team_score[m['team2']]['q8'] += 1
+            if code == '1M':
+                c = '1M'
+            team_score[m['winner']][c] += 1
+
+print_team_score(team_score)

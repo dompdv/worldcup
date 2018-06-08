ca08c38c5b7335b3cfb60155ea0e9c4e59b8f37d
Dominique Poudevigne <dompdv@yahoo.fr>
2018-06-08 06:44:48 +0200

    Mise à jour au 08/06 des ELO Ajout de statistiques supplémentaires

--- worldcup_2018_np.py
+++ worldcup_2018_np.py
@@ -170,16 +170,31 @@ def print_team_score(team_score, n_tir):
         print()
 
 def print_pool_result_stats(stats, n_tir):
-    print("Date       |    Code    | Team1             | Team2             |  1  |  N  |  2  |  p1   |  pN   |  p2   |  c1   |  cN   |  C2   ")
+    print("Date       |    Code    | Team1             | Team2             |  1  |  N  |  2  |  p1   |  pN   |  p2   |  c1   |  cN   |  C2   |  1G   |  2G   |")
     for code, match in stats.items():
         team1 = match['team1']
         team2 = match['team2']
         m1, mn, m2 = match['1'], match['N'], match['2']
-        print("{:^11}|{:^12}|{:^19}|{:^19}|{:^5}|{:^5d}|{:^5d}|{:^7.1f}|{:^7.1f}|{:^7.1f}|{:^7.1f}|{:^7.1f}|{:^7.1f}|".format(
+        g1, g2 = match['1GD'], match['2GD']
+        print("{:^11}|{:^12}|{:^19}|{:^19}|{:^5}|{:^5d}|{:^5d}|{:^7.1f}|{:^7.1f}|{:^7.1f}|{:^7.1f}|{:^7.1f}|{:^7.1f}|{:^7.1f}|{:^7.1f}|".format(
             match['date'], code, team1, team2, m1, mn, m2,
             100 * m1/n_tir, 100 * mn / n_tir, 100 * m2/n_tir,
-            n_tir / m1 if m1 > 0 else 0, n_tir / mn  if mn > 0 else 0, n_tir / m2 if m2 > 0 else 0)
+            n_tir / m1 if m1 > 0 else 0, n_tir / mn  if mn > 0 else 0, n_tir / m2 if m2 > 0 else 0,
+            g1 / m1 if m1 > 0 else 0, g2 / m2 if m2 > 0 else 0)
         )
+    print()
+
+def print_group_winner_stats(winners, n_tir, groups):
+    for group in "ABCDEFGH":
+        countries = sorted(groups[group])
+        print("Group {:^2}{:^8}|{:^18}|{:^18}|{:^18}|{:^18}|".format(group, "", *countries))
+        for i in range(2):
+            print("{:^8}{:^8}|".format("", "First" if i == 0 else "Second"), end='')
+            for country in countries:
+                s = winners[i][group][country]
+                print("{:^6d}{:^6.0f}{:^6.1f}|".format(s, 100 * s / n_tir, n_tir / s if s > 0 else 0), end='')
+            print()
+
     print()
 
 
@@ -230,10 +245,11 @@ model.print(teams)
 probabilities = model.probabilities.copy()
 
 team_score = {team: {"1": 0, "1M": 0, "2": 0, "4": 0, "8": 0, "q8": 0} for team in teams}
-pool_match_stats = {m['match'] : {'date': m['date'], 'team1':m['team1'], 'team2':m['team2'], '1':0, 'N':0, '2':0}
+pool_match_stats = {m['match'] : {'date': m['date'], 'team1':m['team1'], 'team2':m['team2'], '1':0, 'N':0, '2':0, '1GD':0, '2GD':0}
                   for m in matches if ord(m['match'][0]) > 64}
+pool_winner_stats = [{group: {country:0 for country in countries} for group, countries in groups.items()} for _ in range(2)]
 n_tir = 0
-for _ in range(10000):
+for _ in range(100):
     n_tir += 1
     print("Shot n:{}".format(n_tir))
     model.probabilities = probabilities.copy()
@@ -253,6 +269,9 @@ for _ in range(10000):
                 # Les équipes ont passé les pool
                 team_score[m['team1']]['q8'] += 1
                 team_score[m['team2']]['q8'] += 1
+                group1, group2 = code[1], code[2]
+                pool_winner_stats[0][group1][m['team1']] += 1
+                pool_winner_stats[1][group2][m['team2']] += 1
             if code == '1M':
                 c = '1M'
             team_score[m['winner']][c] += 1
@@ -262,8 +281,10 @@ for _ in range(10000):
             continue
         if m['gd'] > 0:
             pool_match_stats[code]['1'] += 1
+            pool_match_stats[code]['1GD'] += m['gd']
         elif m['gd'] < 0:
             pool_match_stats[code]['2'] += 1
+            pool_match_stats[code]['2GD'] -= m['gd']
         else:
             pool_match_stats[code]['N'] += 1
 
@@ -271,3 +292,4 @@ for _ in range(10000):
 model.print(teams)
 print_team_score(team_score, n_tir)
 print_pool_result_stats(pool_match_stats, n_tir)
+print_group_winner_stats(pool_winner_stats, n_tir, groups)
